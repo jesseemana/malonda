03b8a5bdb6400a73029a44ace34049a0576007f9
Jesse Emana <jesseemana@gmail.com>
2024-06-24 17:21:35 +0200

    minor changes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import axios from '../api/axios'
 import Checkout from '../components/Checkout'
-import { Button } from '../components/ui/button'
 import { BeatLoader } from 'react-spinners'
 import { useQuery } from 'react-query'
+import { Button } from '../components/ui/button'
 import { Dialog, DialogTrigger, DialogContent, } from '../components/ui/dialog'
 
 type ProductType = {
@@ -23,34 +23,35 @@ const Home = () => {
   
   return (
     <main className='py-20 px-4'>
-      {isLoading ? <Loader /> : 
-      <div className='flex flex-col md:grid grid-cols-3 gap-4'>
-        {products && products.map((product) => (
-          <div key={product._id} className='flex flex-col'>
-            <img 
-              src={product.image} 
-              alt='product image' 
-              className='md:h-[260px] bg-neutral-50'
-            />
-            <div className='p-2 space-y-2'>
-              <p className='flex text-2xl capitalize font-semibold justify-between'>
-                <span>{product.name}</span>
-                <span>MWK{(product.price).toFixed(2)}</span>
-              </p>
-              <Dialog>
-                <DialogTrigger>
-                  <Button className='md:w-[420px] w-[380px] text-xl rounded-sm'>
-                    Buy now
-                  </Button>
-                </DialogTrigger>
-                <DialogContent>
-                  <Checkout amount={product.price} />
-                </DialogContent>
-              </Dialog>
+      {isLoading ? <Loader /> : (
+        <div className='flex flex-col md:grid grid-cols-3 gap-4'>
+          {products && products.map((product) => (
+            <div key={product._id} className='flex flex-col'>
+              <img 
+                src={product.image} 
+                alt='product image' 
+                className='md:h-[260px] bg-neutral-50'
+              />
+              <div className='p-2 space-y-2'>
+                <p className='flex text-2xl capitalize font-semibold justify-between'>
+                  <span>{product.name}</span>
+                  <span>MWK{(product.price).toFixed(2)}</span>
+                </p>
+                <Dialog>
+                  <DialogTrigger>
+                    <Button className='md:w-[420px] w-[380px] text-xl rounded-sm'>
+                      Buy now
+                    </Button>
+                  </DialogTrigger>
+                  <DialogContent>
+                    <Checkout amount={product.price} />
+                  </DialogContent>
+                </Dialog>
+              </div>
             </div>
-          </div>
-        ))}
-      </div>}
+          ))}
+        </div>
+      )}
     </main>
   )
 }
